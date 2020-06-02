d65f22d767e190c12519880093ba69bffe111390
Chris Yoon <chy.yoon@gmail.com>
2020-06-02 11:02:18 -0700

    add toggle feature to dom
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $(".loremfade").click(function(){
+    $(".lorem-showing").fadeToggle();
+  });
 });
+
+
